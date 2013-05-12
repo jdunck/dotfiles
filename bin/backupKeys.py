cd75c1358661040830bce3e6b3937d32b39e8105
Jeremy Dunck <jdunck@votizen.com>
2013-05-11 23:26:31 -0700

    key backup tweaks

--- bin/backupKeys.py
+++ bin/backupKeys.py
@@ -11,8 +11,12 @@ def backup(dest):
   ownertrust = subprocess.check_output(["gpg", "--export-ownertrust"] )
   with open(os.path.join(dest, 'gpg-ownertrust'), 'w') as f:
     f.write(ownertrust)
-  for key in local_conf['keys']:
-    shutil.copy(key, dest)
+  for key_type in local_conf['keys']:
+    key_type_dest = os.path.join(dest, key_type)
+    if not os.path.exists(key_type_dest):
+      os.makedirs(key_type_dest)
+    for source_file in local_conf['keys'][key_type]:
+      shutil.copy(source_file, key_type_dest)
 
 if __name__ == '__main__':
   import sys
